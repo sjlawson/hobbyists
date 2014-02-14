eee15be2be4c545303a29ecb85f2ed7784970f27
Samuel Lawson <sjlawson@sdf.org>
2014-02-14 11:45:48 -0500

    added template from htmltemplates.net

---
+++ hobbyists.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html><head><input>
+  
+  <meta charset="UTF-8">
+  <title>Hobbyist Manager</title>
+
+  
+  
+  <meta name="description" content="Description of your site goes here">
+
+  
+  <meta name="keywords" content="keyword1, keyword2, keyword3">
+
+  
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+
+</head><body>
+<div class="wrapper">
+<div class="left-gap1"><img src="images/spacer.gif" alt="#" border="0" height="1" width="6"></div>
+<div class="main">
+<div class="header">
+<div class="banner-area"><img src="images/banner-1.jpg" alt="#" border="0"></div>
+</div>
+<div class="mid">
+<div class="mid-left"><br>
+</div>
+<div class="mid-right">
+<h1>Manage Hobbyists</h1>
+<br>
+First name: <input name="firstname"></div>
+</div>
+<div class="footer">
+<p class="copyright-text">Copyright 2010. Designed by <a target="_blank" href="http://www.htmltemplates.net/">htmltemplates.net</a></p>
+</div>
+</div>
+<div class="right-gap1"><img src="images/spacer.gif" alt="#" border="0" height="1" width="6"></div>
+</div>
+
+&nbsp;<a href="http://www.htmltemplates.net">
+<img src="images/footnote.gif" class="copyright" alt="html templates"></a>&nbsp;<a href="http://websitetemplates.net">
+<img src="images/footnote.gif" class="copyright" alt="websitetemplates.net"></a>&nbsp;
+</body></html>
